431d4085367730f25e35b483ea27ae730b22b64f
Keon <keonk87@icloud.com>
2024-07-26 16:01:12 -0400

    added signup and login routes, added session authentication (stored in mongo)

--- backend/src/Controllers/UserController.ts
+++ backend/src/Controllers/UserController.ts
@@ -1,11 +1,56 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import { User } from "../Models/UserModel";
 
 export default function UserController(): express.Router {
   const router = express.Router();
 
-  router.post("/signup", async (req, res) => {});
+  router.post("/signup", async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
 
-  router.post("/login", async (req, res) => {});
+      if (!username || !password) {
+        throw new Error("Missing username or password");
+      }
+
+      const hashedPassword = bcrypt.hashSync(password);
+
+      await User.create({ username, password: hashedPassword });
+
+      res.send("User successfully created");
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post("/login", async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+
+      if (!username || !password) {
+        throw new Error("Missing username or password");
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new Error("Account does not exist");
+      }
+
+      const passwordMatch = bcrypt.compareSync(password, user.password);
+
+      if (!passwordMatch) {
+        throw new Error("Password does not match");
+      }
+
+      //@ts-ignore
+      req.session.user = user._id;
+
+      res.send("User logged in");
+    } catch (err) {
+      next(err);
+    }
+  });
 
   return router;
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import UserController from "./Controllers/UserController";
+import ProductController from "./Controllers/ProductController";
 
 async function main() {
   const app = express();
   dotenv.config();
-  const PORT = process.env.PORT
-  const MONGO_URI = process.env.MONGO_URI
+  const PORT = process.env.PORT;
+  const MONGO_URI = process.env.MONGO_URI;
 
   await mongoose.connect(MONGO_URI);
 
   app.use(express.json());
 
-  app.get("/", (req, res, next) => {
-    res.send("Hello World!");
-  });
+  app.use(
+    session({
+      secret: "my secret",
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: MONGO_URI,
+      }),
+      cookie: {
+        secure: false,
+      },
+    })
+  );
 
+  app.use("/", UserController());
+  app.use("/product", ProductController());
 
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
